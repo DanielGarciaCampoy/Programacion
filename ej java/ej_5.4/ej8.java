b0efdeac0a93b1a48dcee94dd63383f65a7378da
Daniel García Campoy <72436388+DanielGarciaCampoy@users.noreply.github.com>
2020-12-02 09:42:17 +0100

    Add files via upload

---
+++ ej java/ej_5.4/ej8.java
@@ -0,0 +1,31 @@
+/** 
+ * ej8 tema 5
+ * DGC
+*/
+public class ej8 {
+  public static void main(String[] args) {
+    
+    System.out.print("Tabla de multiplicar del número: ");
+    int n = Integer.parseInt(System.console().readLine());
+
+    //tabla de multiplicar
+    System.out.println( n + " * " + "1 = " + n);
+    int n2 = n * 2;
+    System.out.println( n + " * " + "2 = " + n2);
+    int n3 = n * 3;
+    System.out.println( n + " * " + "3 = " + n3);
+    int n4 = n * 4;
+    System.out.println( n + " * " + "4 = " + n4);
+    int n5 = n * 5;
+    System.out.println( n + " * " + "5 = " + n5);
+    int n6 = n * 6;
+    System.out.println( n + " * " + "6 = " + n6);
+    int n7 = n * 7;
+    System.out.println( n + " * " + "7 = " + n7);
+    int n8 = n * 8;
+    System.out.println( n + " * " + "8 = " + n8);
+    int n9 = n * 9;
+    System.out.println( n + " * " + "9 = " + n9);
+
+  }
+}
